2d599044e5ba11d2f8983776a835f0f89ed81814
Lowkik-Sai <lowkiksaipotnuru@gmail.com>
2024-05-08 22:30:59 +0530

    Edited GraphADT

--- molecule-visulaiser/src/components/GraphADT.js
+++ molecule-visulaiser/src/components/GraphADT.js
@@ -9,21 +9,40 @@ export class atomNode {
 
 export class Molecule {
     constructor() {
-        this.adjacenyMatrix = [];
+        this.adjacencyList={};
     }
 
 
-    getNeighbours() {
-
-    }
-
-    
-    addAtoms() {
-
+    getNeighbours(atom) {
+        return this.adjacencyList[atom.atomName];
     }
 
     
-    addBond() {
+    addAtoms(atom) {
+        if(!this.adjacencyList[atom.atomName]){
+            this.adjacencyList[atom.atomName] = [];
+        }else{
+            console.log("Atom exists already in graph")
+        }
+    }
 
+    
+    addBond(atom1,atom2) {
+        if(this.adjacencyList[atom1.atomName] && this.adjacencyList[atom2.atomName]){
+            let flag1=0;
+            for (i in adjacencyList[atom1.atomName]){
+                if(i==atom2.atomName) flag1=1;
+            }
+            if(!flag1) this.adjacencyList[atom1.atomName].push(atom2.atomName);
+
+            let flag2=0;
+            for (i in adjacencyList[atom2.atomName]){
+                if(i==atom1.atomName) flag2=1;
+            }
+            if(!flag2) this.adjacencyList[atom2.atomName].push(atom1.atomName);
+        }
+        else{
+            console.log("Please Add Atoms first");
+        }
     }
 }
