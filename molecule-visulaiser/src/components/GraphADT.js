e3b4b4881d9477390f97b632afe55a7217a5a29c
Naganathan05 <umaiyalramesh@gmail.com>
2024-05-08 21:52:52 +0530

    GraphtADT Template Upadated

--- molecule-visulaiser/src/components/GraphADT.js
+++ molecule-visulaiser/src/components/GraphADT.js
@@ -1,3 +1,29 @@
-class Graph{
+export class atomNode {
+    constructor(atomName, hybridisation, atomSymbol) {
+        this.atomName = atomName;
+        this.hybridisation = hybridisation;
+        this.atomSymbol = atomSymbol;
+        this.coordinates = [0, 0, 0];
+    }
+}
+
+export class Molecule {
+    constructor() {
+        this.adjacenyMatrix = [];
+    }
+
+
+    getNeighbours() {
+
+    }
+
     
+    addAtoms() {
+
+    }
+
+    
+    addBond() {
+
+    }
 }
